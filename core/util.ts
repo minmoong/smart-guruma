f84d00a875470f0c1d4951b782f56304937d0dba
minmoong <kim774634@gmail.com>
2024-09-07 15:33:19 +0900

    Introduce haversine

--- core/util.ts
+++ core/util.ts
@@ -1,11 +1,14 @@
-export const getDistance = (
-  lat1: number,
-  lon1: number,
-  lat2: number,
-  lon2: number
-) => {
-  const xDiff = (lon2 - lon1) * Math.cos((((lat1 + lat2) / 2) * Math.PI) / 180); // 경도 보정
-  const yDiff = lat2 - lat1;
-  const distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff) * 111320; // 위도/경도 -> 미터로 변환
-  return distance;
+import { TCoords } from '@/types';
+import haversine from 'haversine';
+
+export const getDistance = (userCoords: TCoords, destCoords: TCoords) => {
+  const start = {
+    latitude: userCoords.latitude,
+    longitude: userCoords.longitude,
+  };
+  const end = {
+    latitude: destCoords.latitude,
+    longitude: destCoords.longitude,
+  };
+  return haversine(start, end, { unit: 'meter' });
 };
